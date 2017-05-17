639cc296dc62182e9131869a12bfe1e17bcc4d15
PioneerAxon <arth.svnit@gmail.com>
2017-05-17 19:18:39 +0530

    Add simple test to verify the code.

---
+++ tests/single-thread-stress.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 Arth Patel (PioneerAxon) <arth.svnit@gmail.com>
+ *
+ * This file is part of MallocTrack.
+ *
+ * MallocTrack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MallocTrack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	void* mem;
+	for (int i = 0; i < 1000000; i++)
+	{
+		mem = malloc(i);
+		printf("%d %p\n", i, mem);
+		free(mem);
+	}
+	return 0;
+}
